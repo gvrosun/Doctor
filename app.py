9b1b41a53ce2d2d30bcc7b8776aba5438f810a86
Rosun GV <47551990+gvrosun@users.noreply.github.com>
2021-07-15 21:53:40 +0530

    Added 404 page
    Fixed Login
    Fixed Register
    Added 404 page

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template
+from flask_login import login_user, login_required, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from forms import LoginForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "key"
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        print(email, password)
+
+    return render_template('Login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('SignUp.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
 
 
 if __name__ == '__main__':
