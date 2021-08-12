69f7ec5c3ee3bb8458ffe192d9807b7ffa7191f0
Rosun GV <47551990+gvrosun@users.noreply.github.com>
2021-08-12 11:09:29 +0530

    Sensitive data protected

--- mydoctor/__init__.py
+++ mydoctor/__init__.py
@@ -10,14 +10,14 @@ from flask_mobility import Mobility
 login_manager = LoginManager()
 basedir = os.path.dirname(__file__)
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "key_for_doctor_project"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), './uploads/')
 app.config['MAIL_SERVER'] = 'smtp.mailgun.org'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'postmaster@sandboxad62627b546b40469230313f9275ed42.mailgun.org'
-app.config['MAIL_PASSWORD'] = 'c98dcf595789faeeb9a534a6fc00058d-95f6ca46-fb603069'
+app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = 'gvrosun@gmail.com'
 
 db = SQLAlchemy(app)
